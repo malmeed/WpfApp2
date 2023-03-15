3d81eb132f5713bcbf4627e72b147ef3a42edbeb
user <user@526-10>
2023-03-15 09:08:55 +0400

    Добавьте файлы проекта.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private ObservableCollection<Teacher> _data;
+
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            _data = new ObservableCollection<Teacher>();
+
+            dt_grid.ItemsSource = _data;
+
+            var newwindows = new Window1();
+
+            newwindows.ShowDialog();
+            var new_object = newwindows.GetNewObject();
+
+            _data.Add(new_object);
+        }
+
+        private void btn_del_Click(object sender, RoutedEventArgs e)
+        {
+            var s = (e.OriginalSource as Button).DataContext;
+            Teacher current = (Teacher)s;
+            _data.Remove(current);
+            //_data.Remove((Teacher)dt_grid.SelectedItem);
+
+        }
+        public class Teacher
+
+        {
+            public string Name { get; set; }
+            public string ContactNumber { get; set; }
+        }
+    }
+}
---
+++ WpfApp2/Window1.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using static WpfApp2.MainWindow;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+
+        }
+
+        public Teacher GetNewObject()
+        {
+            return new Teacher() { Name = "Новый предмет", ContactNumber = "новый номер" };
+        }
+    }
+}
